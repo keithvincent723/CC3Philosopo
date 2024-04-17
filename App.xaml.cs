808b693da442a862a2451ccd3f89c828bf948eec
keithvincent723 <156585340+keithvincent723@users.noreply.github.com>
2024-04-17 12:49:47 +0800

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CC31N_TeamWM;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
